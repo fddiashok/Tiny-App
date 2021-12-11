67af2ffaf8ae437f7d31a37984aaf407702d85bf
fddiashok <fddiashok@gmail.com>
2021-12-11 10:05:40 -0500

    steup server

---
+++ express_server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
